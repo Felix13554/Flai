1f1789a566012ed8bcc36c994fa546386e6f2e83
Felix Berg <mail@flai.dk>
2026-09-07 13:05:33 +0200

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1059,7 +1059,7 @@ const Section_9009b281 = ((
         </section>
       );
     };
-  return (() => null) as React.ComponentType;
+return Testimonials;
 })(
   React.useState, React.useEffect, React.useRef, React.useMemo, React.useLayoutEffect
 ) as React.ComponentType;
